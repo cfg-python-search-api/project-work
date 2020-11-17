9c8239112a6ef7bc91423e743e3c396dbe6d06d4
Roshan <grantroshan@gmail.com>
2020-11-17 20:36:10 +0000

    updated version

--- test.py
+++ test.py
@@ -1,30 +1,30 @@
 import requests
 
-def recipe_search(ingredient):
+def recipe_search(ingredient, diet, health):
     app_id= '9faba4be'
     app_key = '0b2e438f001911bfb8bbb24baa8dac41'
-    url = f'https://api.edamam.com/search?q={ingredient}&app_id={app_id}&app_key={app_key}'
-
-
-
-
+    url = f'https://api.edamam.com/search?q={ingredient}&app_id={app_id}&app_key={app_key}&diet={diet}&health={health}'
 
     response = requests.get(url)
 
     data = response.json()
     return data['hits']
 
-recipe_search('cheese')
 
 def run():
    ingredient = input('Enter an ingredient: ')
-   results = recipe_search(ingredient)
+   diet = input('Enter diet requirement: ')
+   health = input('Enter any health requirements: ')
+   results = recipe_search(ingredient, diet, health)
 
    for result in results:
        recipe = result['recipe']
 
+       print(f"The following recipes contain(s) {ingredient} and give you a {diet} diet and is suitable for a {health} diet")
+       print()
        print(recipe['label'])
        print(recipe['url'])
-       print()
+       print("This recipe contains " +  str(int(recipe['calories'])) + " calories")
+
 
 run()
