eec1a0367c707fbd2348ee0e3a4ab731c2d6742b
leah1212 <74267871+leah1212@users.noreply.github.com>
2020-11-24 17:26:25 +0000

    Add files via upload

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 import requests
 
-def recipe_search(ingredient, diet, health):
+def recipe_search(ingredient, diet):
     app_id= '9faba4be'
     app_key = '0b2e438f001911bfb8bbb24baa8dac41'
-    url = f'https://api.edamam.com/search?q={ingredient}&app_id={app_id}&app_key={app_key}&diet={diet}&health={health}'
+    url = f'https://api.edamam.com/search?q={ingredient}&app_id={app_id}&app_key={app_key}&diet={diet}'
 
     response = requests.get(url)
 
@@ -12,19 +12,17 @@ def recipe_search(ingredient, diet, health):
 
 
 def run():
-   ingredient = input('Enter an ingredient: ')
-   diet = input('Enter diet requirement (balanced/high-protein/ high-fiber/ low-fat/ low-sodium : ')
-   health = input('Enter any health requirements: ')
-   results = recipe_search(ingredient, diet, health)
+   ingredient = input('Enter 1 or more ingredients: ')
+   diet = input('Enter diet requirement (balanced/high-protein/ high-fiber/ low-fat/ low-sodium) : ')
+   results = recipe_search(ingredient, diet)
 
    for result in results:
        recipe = result['recipe']
 
-       print(f"The following recipes contain(s) {ingredient} and give you a {diet} diet and is suitable for a {health} diet")
+       print(f"The following recipes contain(s) {ingredient} and give you a {diet} diet.")
        print()
        print(recipe['label'])
        print(recipe['url'])
        print("This recipe contains " +  str(int(recipe['calories'])) + " calories")
-
 
 run()
