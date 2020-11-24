47b8fc8f6322360c5fc2ce0e0eab33654d342d15
leah1212 <74267871+leah1212@users.noreply.github.com>
2020-11-24 17:39:29 +0000

    Add files via upload

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import requests
 
-def recipe_search(ingredient, diet):
+def recipe_searchy(ingredient, diet):
     app_id= '9faba4be'
     app_key = '0b2e438f001911bfb8bbb24baa8dac41'
     url = f'https://api.edamam.com/search?q={ingredient}&app_id={app_id}&app_key={app_key}&diet={diet}'
@@ -10,19 +10,40 @@ def recipe_search(ingredient, diet):
     data = response.json()
     return data['hits']
 
+def recipe_searchn(ingredient):
+    app_id= '9faba4be'
+    app_key = '0b2e438f001911bfb8bbb24baa8dac41'
+    url = f'https://api.edamam.com/search?q={ingredient}&app_id={app_id}&app_key={app_key}'
+
+    response = requests.get(url)
+
+    data = response.json()
+    return data['hits']
+
 
 def run():
-   ingredient = input('Enter 1 or more ingredients: ')
-   diet = input('Enter diet requirement (balanced/high-protein/ high-fiber/ low-fat/ low-sodium) : ')
-   results = recipe_search(ingredient, diet)
+    ingredient = input('Enter 1 or more ingredients: ')
+    diet_yn = input('Do you have any dietary requirements? (Y/N)')
+    if diet_yn == 'Y':
+        diet = input('Enter diet requirement (balanced/high-protein/ high-fiber/ low-fat/ low-sodium) : ')
+        results = recipe_searchy(ingredient, diet)
+        for result in results:
+            recipe = result['recipe']
 
-   for result in results:
-       recipe = result['recipe']
+            print(f"The following recipes contain(s) {ingredient} and give you a {diet} diet.")
+            print()
+            print(recipe['label'])
+            print(recipe['url'])
+            print("This recipe contains " + str(int(recipe['calories'])) + " calories")
+    elif diet_yn == 'N':
+        results = recipe_searchn(ingredient)
+        for result in results:
+            recipe = result['recipe']
 
-       print(f"The following recipes contain(s) {ingredient} and give you a {diet} diet.")
-       print()
-       print(recipe['label'])
-       print(recipe['url'])
-       print("This recipe contains " +  str(int(recipe['calories'])) + " calories")
+            print(f"The following recipes contain(s) {ingredient}.")
+            print()
+            print(recipe['label'])
+            print(recipe['url'])
+            print("This recipe contains " + str(int(recipe['calories'])) + " calories")
 
 run()
