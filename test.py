2aa98eb3a23725b26146683ba4519e1311cf1094
leah1212 <74267871+leah1212@users.noreply.github.com>
2020-11-24 19:09:14 +0000

    Add files via upload

--- test.py
+++ test.py
@@ -22,25 +22,25 @@ def recipe_searchn(ingredient):
 
 
 def run():
-    ingredient = input('Enter 1 or more ingredients: ')
+    ingredient = input('Welcome to MAKE WASTE WORK FOR IT: A Waste-Reducing Recipe Generator. Enter 1 or more ingredients: ')
     diet_yn = input('Do you have any dietary requirements? (Y/N)')
     if diet_yn == 'Y':
         diet = input('Enter diet requirement (balanced/high-protein/ high-fiber/ low-fat/ low-sodium) : ')
         results = recipe_searchy(ingredient, diet)
+        print(f"The following recipes contain(s) {ingredient} and give you a {diet} diet. Thank you for reducing food waste!")
         for result in results:
             recipe = result['recipe']
 
-            print(f"The following recipes contain(s) {ingredient} and give you a {diet} diet.")
             print()
             print(recipe['label'])
             print(recipe['url'])
             print("This recipe contains " + str(int(recipe['calories'])) + " calories")
     elif diet_yn == 'N':
         results = recipe_searchn(ingredient)
+        print(f"The following recipes contain(s) {ingredient}. Thank you for reducing food waste!")
         for result in results:
             recipe = result['recipe']
 
-            print(f"The following recipes contain(s) {ingredient}.")
             print()
             print(recipe['label'])
             print(recipe['url'])
